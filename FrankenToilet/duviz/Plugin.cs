39960a8aac38c7b3a660e0526f9e37572f68b588
Duviz 2 <javierduviz2@gmail.com>
2026-02-03 22:20:50 +0100

    Scene changer for a lot of levels

--- FrankenToilet/duviz/Plugin.cs
+++ FrankenToilet/duviz/Plugin.cs
@@ -21,6 +21,10 @@ public static class Plugin
 
         m.AddComponent<Bundle>();
         m.AddComponent<Jesus>();
+        m.AddComponent<Meow>();
+        m.AddComponent<EventsManager>();
+        m.AddComponent<EventsCreator>();
+        m.AddComponent<HealthRemover>();
     }
 
     public static T Ass<T>(string path) { return Addressables.LoadAssetAsync<T>((object)path).WaitForCompletion(); }
